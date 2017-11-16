4a5b98ad84f48da40a01519c6a2f92a98e4ddcaa
Jonathan Lee <jonathan.h.lee@gmail.com>
2017-11-16 03:34:17 -0800

    Assignment 14 models completed

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  let(:question) { Question.create!(title: "New Question", body: "New Question Body", resolved: false) }
+  let(:answer) { Answer.create!(question: question, body: "New Answer Body") }
+
+  it "should respond to body" do
+    expect(answer).to respond_to(:body)
+  end
+end
