027ecdea8490dd35085fbdef652ec85f33859dba
Jonathan Lee <jonathan.h.lee@gmail.com>
2017-12-21 12:32:11 -0800

    Checkpoint 30 updated: user_spec.rb fixed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -71,35 +71,35 @@ RSpec.describe User, type: :model do
       end
     end
   end
-end
 
-describe "invalid user" do
-  let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
-  let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
+  describe "invalid user" do
+    let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
+    let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
 
-  it "should be an invalid user due to blank name" do
-    expect(user_with_invalid_name).to_not be_valid
-  end
-
-  it "should be an invalid user due to blank email" do
-    expect(user_with_invalid_email).to_not be_valid
-  end
-
-  describe "#favorite_for(post)" do
-    before do
-      topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
-      @post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+    it "should be an invalid user due to blank name" do
+      expect(user_with_invalid_name).to_not be_valid
     end
 
-    it "returns `nil` if the user has not favorited the post" do
-      expect(user.favorite_for(@post)).to be_nil
+    it "should be an invalid user due to blank email" do
+      expect(user_with_invalid_email).to_not be_valid
     end
 
-    it "returns the appropriate favorite if it exists" do
-      # create favorite for user and @post
-      favorite = user.favorites.where(post: @post).create
-      # expect favorite_for will return favorite in above line
-      expect(user.favorite_for(@post)).to eq(favorite)
+    describe "#favorite_for(post)" do
+      before do
+        topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+        @post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+      end
+
+      it "returns `nil` if the user has not favorited the post" do
+        expect(user.favorite_for(@post)).to be_nil
+      end
+
+      it "returns the appropriate favorite if it exists" do
+        # create favorite for user and @post
+        favorite = user.favorites.where(post: @post).create
+        # expect favorite_for will return favorite in above line
+        expect(user.favorite_for(@post)).to eq(favorite)
+      end
     end
   end
 end
